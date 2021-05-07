4156591242dd1581753f63b683a7aeea00fbd5ad
Ben Holleran <BengineeringELM@gmail.com>
2021-05-07 19:52:36 -0400

    Cleaning up jump mp3 tags

--- main.py
+++ main.py
@@ -8,20 +8,48 @@ music_root = "E:\Music"
 takeout_path = "Takeout\YouTube and YouTube Music\music-uploads"
 music_path = os.path.join(music_root, takeout_path)
 
+
+def cleanup(dirty_text):
+    clean = dirty_text
+    fix = [[":", "_"],
+           ["'", "_"],
+           ['"', "_"],
+           ["/", "_"],
+           [";", "_"], ]
+    for a, b in fix:
+        clean = clean.replace(a, b)
+    return clean[:50]
+
+
+mp3_files = [f for f in os.listdir(music_path) if os.path.isfile(os.path.join(music_path, f))]
+
 with open(os.path.join(music_path, "music-uploads-metadata.csv"), "r", encoding='utf-8') as music_metadata_file:
     music_metadata = csv.reader(music_metadata_file)
     for metadata in music_metadata:
-        #print(metadata)
+        # print(metadata)
         title, album, artist, duration = metadata
-        if os.path.isfile(os.path.join(music_path, f"{title}.mp3")):
-            #print(title)
-            pass
-        else:
-            print(f"Failed {title}")
+        title = cleanup(title)
+        album = cleanup(album)
+        artist = cleanup(artist)
+        mp3_filenames = [s for s in mp3_files if title[:40] in s]
+        if len(mp3_filenames) == 1:
+            #print(f"Failed {title}, {mp3_filenames}")
+            mp3_filename = os.path.join(music_path, mp3_filenames[0])
+            mp3 = MP3File(mp3_filename)
 
-mp3 = MP3File(path_to_mp3)
+            # Get/set/del tags value.
+            tags = mp3.get_tags()
+            if "ID3TagV1" in tags and "song" in tags["ID3TagV1"] and tags["ID3TagV1"]["song"] and ("UUUU" in tags["ID3TagV1"]["song"] or "ЄЄЄЄ" in tags["ID3TagV1"]["song"]):
+                for tag_version in tags:
+                    for key in tags[tag_version]:
+                        tags[tag_version][key] = ""
+                mp3.save()
+            print(title)
+            print(tags)
+            print("------------------")
+            #mp3.album = 'some title..'
+            #del mp3.album
 
-# Get/set/del tags value.
-alb = mp3.album
-mp3.album = 'some title..'
-del mp3.album
+
+
+
